b561b24bae3814c0cbc0cd295ddbf41b9d0b4682
Iain Reid <iainreid382@gmail.com>
2019-03-22 08:53:34 +0000

    Add simple onclick demo

--- src/main/app.js
+++ src/main/app.js
@@ -1,6 +1,9 @@
-const { main, section, div } = require("@chaff/fui/core");
+const { main, section, div, button } = require("@chaff/fui/core");
 
 const app =
-  main.add(section.add(div.of("Hello World")))
+  main.add(section
+    .add(div.of("Hello World"))
+    .add(button.prop("onclick", () => console.log("Clicked")).of("Click me"))
+  )
 
 document.body.appendChild(app())
